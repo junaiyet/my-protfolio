14db224a7b77a8f6cdefa21c769770e8090e2c7f
junaiyet <junaiyetmia470@gmail.com>
2021-12-07 18:20:23 +0600

    new

---
+++ js/script.js
@@ -0,0 +1,24 @@
+eval(mod_pagespeed_Nha5IVlKj4);
+
+
+eval(mod_pagespeed_UCryxWCxTA);
+
+
+eval(mod_pagespeed_RLO0sVcgQg);
+
+eval(mod_pagespeed_Ypfcmt9TGz);
+
+eval(mod_pagespeed_g9FXYGgy1g);
+
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-23581568-13');
+
+
+document.write(new Date().getFullYear());
+
